1efdc470508dbf1b757b0e099a3e128a32f9a4d8
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-01 08:49:18 -0500

    Add files via upload
    Using octothorpes to explain what I did in each line.

--- learning.py
+++ learning.py
@@ -1,11 +1,21 @@
+# this is an octothorpe -- it makes a comment (line ignored by the computer)
 # print("Hello world")
+# writing random stuff
 print("Howdy, y'all")
 print("I like typing this")
 print("This is fun")
+
 # math skills demo
-print("I not count my chickens", 2)
+
+# makes a number appear
+print("I will not count my chickens", 2)
+# does math for you, 30 divided by 6 equals 5 plus 25 equals 30, shows the answer by the hens
 print("Hens: ", 25 + 30 / 6)
+# does math for you again, the percent sign shows remainders with no fractions
+# 25 times 3 equals 75.75 % 4 equals 3.1 minus 3 equals 97
 print("Roosters: ", 100 - 25 * 3 % 4)
 print("Now I will count the eggs", 7)
+# 3 plus 2 plus 1 equals 6 minus 5 equals 1. 4 & 2 equals 0 plus 6 equals 6. 1 minus 0.25 plus 6 equals 6.75.
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# 3 plus 2 equals 5 and 5 minus 7 equals -2. -2 is not greater than 5 so python shows this as false.
 print(3 + 2 < 5 - 7)
