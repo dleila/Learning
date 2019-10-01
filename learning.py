7a96cf4ff9b58d446fbf66dfb871bc5665ad872d
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-01 09:01:59 -0500

    Add files via upload
    My math seems right, I don't get what the floating numbers thing is referencing.

--- learning.py
+++ learning.py
@@ -2,21 +2,24 @@
 # print("Hello world")
 # writing random stuff
 print("Howdy, y'all")
+# more text
 print("I like typing this")
+# even more text
 print("This is fun")
 
 # math skills demo
 
-# makes a number appear
+# makes a number appear by the text
 print("I will not count my chickens", 2)
 # does math for you, 30 divided by 6 equals 5 plus 25 equals 30, shows the answer by the hens
 print("Hens: ", 25 + 30 / 6)
 # does math for you again, the percent sign shows remainders with no fractions
 # 25 times 3 equals 75.75 % 4 equals 3.1 minus 3 equals 97
 print("Roosters: ", 100 - 25 * 3 % 4)
+# another number appears by the text
 print("Now I will count the eggs", 7)
 # 3 plus 2 plus 1 equals 6 minus 5 equals 1. 4 & 2 equals 0 plus 6 equals 6. 1 minus 0.25 plus 6 equals 6.75.
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 # 3 plus 2 equals 5 and 5 minus 7 equals -2. -2 is not greater than 5 so python shows this as false.
 print(3 + 2 < 5 - 7)
-print(75 - 15 / 8 % 4 + 1/8 - 7)
+print(75.0 - 15.0 / 08.0 > 04.0 + 1/8 - 07.0)
