5c5d5f25b20e058b20f8d428a9cc5299a51a02a6
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-03 08:41:55 -0500

    Add files via upload
    Talked about cars using python.

--- learning.py
+++ learning.py
@@ -24,3 +24,20 @@ print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print(3 + 2 < 5 - 7)
 # 75 minus 15 equals 60. 60 divided by 8 equals 7.5. 4 plus 0.125 minus 7 equals -2.875. 7n5 is greater than -2.875
 print(75.0 - 15.0 / 8.0 > 4.0 + 1/8 - 7.0)
+
+# variables and some  of their powers
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = space_in_a_car * cars_driven
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available today.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put approximately", average_passengers_per_car, "people in each car.")
